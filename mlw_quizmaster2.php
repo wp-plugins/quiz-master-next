65fcf78d9e0e4c8d4bade69b5518c6a5179aa52a
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-10 01:32:05 +0000

    Uploading Initial Version
    git-svn-id: https://plugins.svn.wordpress.org/quiz-master-next/tags/0.1@769498 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mlw_quizmaster2.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+Plugin Name: Quiz Master Next
+Description: This adds a quiz function to your website. This is the new version of the wildly popular Quiz Master.
+Version: 0.1
+Author: Frank Corso
+Author URI: http://www.mylocalwebstop.com/
+Plugin URI: http://www.mylocalwebstop.com/
+*/
+
+/* 
+Copyright 2013, My Local Webstop (email : fpcorso@mylocalwebstop.com)
+
+Disclaimer of Warranties. 
+
+The plugin is provided "as is". My Local Webstop and its suppliers and licensors hereby disclaim all warranties of any kind, 
+express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement. 
+Neither My Local Webstop nor its suppliers and licensors, makes any warranty that the plugin will be error free or that access thereto will be continuous or uninterrupted.
+You understand that you install, operate, and unistall the plugin at your own discretion and risk.
+*/
+
+
+///Files to Include
+include("includes/mlw_main_page.php");
+include("includes/mlw_quiz.php");
+include("includes/mlw_dashboard.php");
+include("includes/mlw_quiz_admin.php");
+include("includes/mlw_quiz_options.php");
+include("includes/mlw_quiz_install.php");
+include("includes/mlw_results.php");
+
+
+///Activation Actions
+add_action('admin_menu', 'mlw_add_menu');
+add_shortcode('mlw_quizmaster', 'mlw_quiz_shortcode');
+//add_action('wp_dashboard_setup', "mlw_add_dashboard_widget"); Commented out until I have a use for it. Creates dashboard widget
+register_activation_hook( __FILE__, 'mlw_quiz_activate');
+register_deactivation_hook( __FILE__, 'mlw_quiz_deactivate');
+
+
+///Create Admin Pages
+function mlw_add_menu()
+{
+	if (function_exists('add_menu_page'))
+	{
+		add_menu_page('Quizzes', 'Quizzes', 8, __FILE__, 'mlw_generate_quiz_admin');
+		add_submenu_page(__FILE__, 'Quiz Options', 'Quiz Options', 8, 'mlw_quiz_options', 'mlw_generate_quiz_options');
+		add_submenu_page(__FILE__, 'Quiz Results', 'Quiz Results', 8, 'mlw_quiz_results', 'mlw_generate_quiz_results');
+		add_submenu_page(__FILE__, 'Support', 'Support', 8, 'mlw_quiz_tools', 'mlw_generate_main_page');
+	}
+}
+/*
+
+
+*/
+?>
