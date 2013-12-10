0340cc4fb6d0b955b64fded88705861ea0682714
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 02:55:18 +0000

    Adding includes folder to 0.8.1 folder
    git-svn-id: https://plugins.svn.wordpress.org/quiz-master-next/tags/0.8.1@817953 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/mlw_quiz_install.php
@@ -0,0 +1,304 @@
+<?php
+/*
+These functions are used for installing and uninstalling all necessary databases, options, page, etc.. for the plugin to work properly.
+*/
+function mlw_quiz_activate()
+{
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "mlw_quizzes";
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) 
+
+	{
+
+		$sql = "CREATE TABLE " . $table_name . " (
+
+			quiz_id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+			quiz_name TEXT NOT NULL,
+			
+			message_before TEXT NOT NULL,
+			
+			message_after TEXT NOT NULL,
+			
+			message_comment TEXT NOT NULL,
+
+			user_email_template TEXT NOT NULL,
+			
+			admin_email_template TEXT NOT NULL,
+			
+			submit_button_text TEXT NOT NULL,
+			
+			name_field_text TEXT NOT NULL,
+			
+			business_field_text TEXT NOT NULL,
+			
+			email_field_text TEXT NOT NULL,
+			
+			phone_field_text TEXT NOT NULL,
+			
+			comment_field_text TEXT NOT NULL,
+
+			system INT NOT NULL,
+
+			show_score INT NOT NULL,
+
+			send_user_email INT NOT NULL,
+
+			send_admin_email INT NOT NULL,
+
+			user_name INT NOT NULL,
+
+			user_comp INT NOT NULL,
+
+			user_email INT NOT NULL,
+
+			user_phone INT NOT NULL,
+
+			admin_email TEXT NOT NULL,
+			
+			comment_section INT NOT NULL,
+
+			quiz_views INT NOT NULL,
+
+			quiz_taken INT NOT NULL,
+
+			deleted INT NOT NULL,
+
+			PRIMARY KEY  (quiz_id)
+
+		);";
+
+		$results = $wpdb->query( $sql );
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+	
+	else
+	
+	{
+		if($wpdb->get_var("SHOW COLUMNS FROM ".$table_name." LIKE 'comment_section'") != "comment_section")
+		{
+			$sql = "ALTER TABLE ".$table_name." ADD comment_field_text TEXT NOT NULL AFTER phone_field_text";
+			
+			$results = $wpdb->query( $sql );
+			
+			$sql = "ALTER TABLE ".$table_name." ADD comment_section INT NOT NULL AFTER admin_email";
+			
+			$results = $wpdb->query( $sql );
+			
+			$sql = "ALTER TABLE ".$table_name." ADD message_comment TEXT NOT NULL AFTER message_after";
+			
+			$results = $wpdb->query( $sql );
+			
+			$update_sql = "UPDATE ".$table_name." SET comment_field_text='Comments', comment_section=1, message_comment='Enter You Text Here'";
+			
+			$results = $wpdb->query( $update_sql );
+		}		
+	}
+
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "mlw_questions";
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) 
+
+	{
+
+		$sql = "CREATE TABLE " . $table_name . " (
+
+			question_id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+			quiz_id INT NOT NULL,
+
+			question_name TEXT NOT NULL,
+			
+			answer_one TEXT NOT NULL,
+			
+			answer_one_points INT NOT NULL,
+
+			answer_two TEXT NOT NULL,
+			
+			answer_two_points INT NOT NULL,
+
+			answer_three TEXT NOT NULL,
+
+			answer_three_points INT NOT NULL,
+
+			answer_four TEXT NOT NULL,
+
+			answer_four_points INT NOT NULL,
+
+			answer_five TEXT NOT NULL,
+
+			answer_five_points INT NOT NULL,
+
+			answer_six TEXT NOT NULL,
+
+			answer_six_points INT NOT NULL,
+
+			correct_answer INT NOT NULL,
+			
+			comments INT NOT NULL,
+			
+			hints TEXT NOT NULL,
+			
+			question_order INT NOT NULL,
+
+			deleted INT NOT NULL,
+
+			PRIMARY KEY  (question_id)
+
+		);";
+
+		$results = $wpdb->query( $sql );
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+	
+	if($wpdb->get_var("SHOW COLUMNS FROM ".$table_name." LIKE 'comments'") != "comments")
+	{
+		$sql = "ALTER TABLE ".$table_name." ADD comments INT NOT NULL AFTER correct_answer";
+		
+		$results = $wpdb->query( $sql );
+		
+		$sql = "ALTER TABLE ".$table_name." ADD hints TEXT NOT NULL AFTER comments";
+		
+		$results = $wpdb->query( $sql );
+		
+		$update_sql = "UPDATE ".$table_name." SET comments=1, hints=''";
+		
+		$results = $wpdb->query( $update_sql );
+	}
+		
+	if($wpdb->get_var("SHOW COLUMNS FROM ".$table_name." LIKE 'question_order'") != "question_order")
+	{
+		$sql = "ALTER TABLE ".$table_name." ADD question_order INT NOT NULL AFTER hints";
+		
+		$results = $wpdb->query( $sql );
+		
+		$update_sql = "UPDATE ".$table_name." SET question_order=0";
+		
+		$results = $wpdb->query( $update_sql );
+		
+	}
+	
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "mlw_results";
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) 
+
+	{
+
+		$sql = "CREATE TABLE " . $table_name . " (
+
+			result_id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+			quiz_id INT NOT NULL,
+
+			quiz_name TEXT NOT NULL,
+
+			quiz_system INT NOT NULL,
+
+			point_score INT NOT NULL,
+
+			correct_score INT NOT NULL,
+
+			correct INT NOT NULL,
+
+			total INT NOT NULL,
+
+			name TEXT NOT NULL,
+
+			business TEXT NOT NULL,
+
+			email TEXT NOT NULL,
+
+			phone TEXT NOT NULL,
+
+			time_taken TEXT NOT NULL,
+			
+			time_taken_real DATETIME NOT NULL,
+			
+			quiz_results TEXT NOT NULL,
+
+			deleted INT NOT NULL,
+
+			PRIMARY KEY  (result_id)
+
+		);";
+
+		$results = $wpdb->query( $sql );
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+	
+	else
+	
+	{
+		if($wpdb->get_var("SHOW COLUMNS FROM ".$table_name." LIKE 'time_taken_real'") != "time_taken_real")
+		{
+			$sql = "ALTER TABLE ".$table_name." ADD time_taken_real DATETIME NOT NULL AFTER time_taken";
+			
+			$results = $wpdb->query( $sql );
+			
+			$sql = "ALTER TABLE ".$table_name." ADD quiz_results TEXT NOT NULL AFTER time_taken_real";
+			
+			$results = $wpdb->query( $sql );
+			
+			$update_sql = "UPDATE ".$table_name." SET quiz_results='This quiz was taken before this plugin began saving answers.', hints=''";
+			
+			$results = $wpdb->query( $update_sql );
+		}		
+	}
+	
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "mlw_qm_audit_trail";
+
+	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) 
+
+	{
+		//Creating the table ... fresh!
+
+		$sql = "CREATE TABLE " . $table_name . " (
+
+			trail_id mediumint(9) NOT NULL AUTO_INCREMENT,
+
+			action_user TEXT NOT NULL,
+			
+			action TEXT NOT NULL,
+			
+			time TEXT NOT NULL,
+
+			PRIMARY KEY  (trail_id)
+
+		);";
+
+		$results = $wpdb->query( $sql );
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+	$data = "0.8.1";
+	if ( ! get_option('mlw_quiz_master_version'))
+	{
+		add_option('mlw_quiz_master_version' , $data);
+	}
+	else
+	{
+		update_option('mlw_quiz_master_version' , $data);
+	}
+}
+
+function mlw_quiz_deactivate()
+{
+
+
+}
+?>
