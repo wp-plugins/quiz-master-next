92bd697898c19cfaba9356850f1298cc4bb40861
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-14 19:40:12 +0000

    Updating to 0.8.3
    git-svn-id: https://plugins.svn.wordpress.org/quiz-master-next/tags/0.8.3@822100 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/mlw_leaderboard.php
@@ -0,0 +1,41 @@
+<?php
+/*
+This function creates the leaderboard from the shortcode.
+*/
+function mlw_quiz_leaderboard_shortcode($atts)
+{
+	extract(shortcode_atts(array(
+		'mlw_quiz' => 0
+	), $atts));
+	$mlw_quiz_id = $mlw_quiz;
+	$mlw_quiz_leaderboard_display = "";
+	
+	
+	global $wpdb;
+	$sql = "SELECT * FROM " . $wpdb->prefix . "mlw_quizzes" . " WHERE quiz_id=".$mlw_quiz_id." AND deleted='0'";
+	$mlw_quiz_options = $wpdb->get_results($sql);
+	foreach($mlw_quiz_options as $mlw_eaches) {
+		$mlw_quiz_options = $mlw_eaches;
+		break;
+	}
+	$sql = "SELECT * FROM " . $wpdb->prefix . "mlw_results WHERE quiz_id=".$mlw_quiz_id." AND deleted='0' LIMIT 10";
+	$mlw_result_data = $wpdb->get_results($sql);
+	
+	$mlw_quiz_leaderboard_display .= "<h3>".$mlw_quiz_options->quiz_name."'s Leaderboard</h3>";
+	$leader_count = 0;
+	foreach($mlw_result_data as $mlw_eaches) {
+		$leader_count++;
+		if ($mlw_quiz_options->system == 0)
+		{
+			$mlw_quiz_leaderboard_display .= "<p>".$leader_count.".&nbsp;".$mlw_eaches->name."&nbsp; - ".$mlw_eaches->correct_score."%</p>";
+		}
+		if ($mlw_quiz_options->system == 1)
+		{
+			$mlw_quiz_leaderboard_display .= "<p>".$leader_count.".&nbsp;".$mlw_eaches->name."&nbsp; - ".$mlw_eaches->point_score." Points</p>";
+		}
+		$mlw_quiz_leaderboard_display .= "<br />";
+	}
+	$mlw_quiz_leaderboard_display .= "<br />";
+	return $mlw_quiz_leaderboard_display;
+}
+?>
