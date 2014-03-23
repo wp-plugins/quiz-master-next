1ebe5873b3ae96c2206e20f98a5a9432123a2d3b
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-23 21:14:50 +0000

    Adding new 2.0.1 tag folder
    git-svn-id: https://plugins.svn.wordpress.org/quiz-master-next/tags/2.0.1@880589 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,31 @@
+<?php
+global $wpdb;
+
+	$table_name = $wpdb->prefix . "mlw_results";
+
+	$sql = "DROP TABLE IF EXISTS ".$table_name;
+
+	$results = $wpdb->query( $sql );
+
+	$table_name = $wpdb->prefix . "mlw_quizzes";
+
+	$sql = "DROP TABLE IF EXISTS ".$table_name;
+
+	$results = $wpdb->query( $sql );
+
+	$table_name = $wpdb->prefix . "mlw_questions";
+
+	$sql = "DROP TABLE IF EXISTS ".$table_name;
+	
+	$results = $wpdb->query( $sql );
+	
+	$table_name = $wpdb->prefix . "mlw_qm_audit_trail";
+
+	$sql = "DROP TABLE IF EXISTS ".$table_name;
+	
+	$results = $wpdb->query( $sql );
+	
+	delete_option('mlw_quiz_master_version');
+	
+	delete_option('mlw_qmn_review_notice');
+?>
