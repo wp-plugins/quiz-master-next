4eaeb00ec9d732e9df0e5a1080a13ab376d7b8b7
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-11 00:04:15 +0000

    Uploading new 2.9.1 tag folder
    git-svn-id: https://plugins.svn.wordpress.org/quiz-master-next/tags/2.9.1@963467 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/mlw_qmn_credits.php
@@ -0,0 +1,148 @@
+<?php
+/*
+This page shows the about page
+*/
+/* 
+Copyright 2014, My Local Webstop (email : fpcorso@mylocalwebstop.com)
+*/
+
+function mlw_generate_about_page()
+{
+	//Page Variables
+	$mlw_quiz_version = get_option('mlw_quiz_master_version');
+	
+	
+	?>
+	<!-- css -->
+	<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/redmond/jquery-ui.css" rel="stylesheet" />
+	<!-- jquery scripts -->
+	<?php
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jquery-ui-core' );
+	wp_enqueue_script( 'jquery-ui-dialog' );
+	wp_enqueue_script( 'jquery-ui-button' );
+	wp_enqueue_script( 'jquery-effects-blind' );
+	wp_enqueue_script( 'jquery-effects-explode' );
+	?>
+	<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>-->
+	<script type="text/javascript">
+		var $j = jQuery.noConflict();
+		// increase the default animation speed to exaggerate the effect
+		$j.fx.speeds._default = 1000;
+		function mlw_qmn_setTab(tab) {
+			jQuery("a.nav-tab-active").toggleClass("nav-tab-active");
+			if (tab == 1)
+			{
+				jQuery("#mlw_quiz_what_new").show();
+				jQuery("#mlw_quiz_changelog").hide();
+				jQuery("#mlw_quiz_requested").hide();
+				jQuery("#mlw_qmn_tab_1").toggleClass("nav-tab-active");
+			}
+			if (tab == 2)
+			{
+				jQuery("#mlw_quiz_what_new").hide();
+				jQuery("#mlw_quiz_changelog").show();
+				jQuery("#mlw_quiz_requested").hide();
+				jQuery("#mlw_qmn_tab_2").toggleClass("nav-tab-active");
+			}
+			if (tab == 3)
+			{
+				jQuery("#mlw_quiz_what_new").hide();
+				jQuery("#mlw_quiz_changelog").hide();
+				jQuery("#mlw_quiz_requested").show();
+				jQuery("#mlw_qmn_tab_3").toggleClass("nav-tab-active");
+			}
+		}
+	</script>
+	<style>
+		div.mlw_qmn_icon_wrap
+		{
+			background: <?php echo 'url("'.plugins_url( 'images/quiz_icon.png' , __FILE__ ).'")'; ?> no-repeat;
+			background: none, <?php echo 'url("'.plugins_url( 'images/quiz_icon.png' , __FILE__ ).'")'; ?> no-repeat;
+			position: absolute; 
+			top: 0; 
+			right: 0; 
+			background-color: #0d97d8;
+			color: yellow;
+			background-position: center 24px;
+			background-size: 85px 85px;
+			font-size: 14px;
+			text-align: center;
+			font-weight: 600;
+			margin: 5px 0 0;
+			padding-top: 120px;
+			height: 40px;
+			display: inline-block;
+			width: 150px;
+			text-rendering: optimizeLegibility;
+			border: 5px solid #106daa;
+			-moz-border-radius: 20px;
+			-webkit-border-radius: 20px;
+			-khtml-border-radius: 20px;
+			border-radius: 20px;
+		}
+	</style>
+	<div class="wrap about-wrap">
+	<h1>Welcome To Quiz Master Next <?php echo $mlw_quiz_version; ?></h1>
+	<div class="about-text">Thank you for updating!</div>
+	<div class="mlw_qmn_icon_wrap">Version <?php echo $mlw_quiz_version; ?></div>
+	<h2 class="nav-tab-wrapper">
+		<a href="javascript:mlw_qmn_setTab(1);" id="mlw_qmn_tab_1" class="nav-tab nav-tab-active">
+			What&#8217;s New In 2.9</a>
+		<a href="javascript:mlw_qmn_setTab(2);" id="mlw_qmn_tab_2" class="nav-tab">
+			Changelog For <?php echo $mlw_quiz_version; ?>	</a>
+		<a href="javascript:mlw_qmn_setTab(3);" id="mlw_qmn_tab_3" class="nav-tab">
+			Requested Features</a>
+	</h2>
+	<div id="mlw_quiz_what_new">
+	<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">Facebook Sharing</h2>
+	<p style="text-align: center;">You users can now share their results with their facebook friends in addition to their twitter friends!</p>
+	<br />
+	<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">New Question Type</h2>
+	<p style="text-align: center;">This update gives you a new question type - Large Open Answer. This type will give you a large text box for your users to fill in as opposed to the smaller text field for the regular open answer.</p>
+	<br />
+	<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">Edit The Email's Subject</h2>
+	<p style="text-align: center;">You can now edit the subject of the email sent to your user after they submit your form.</p>
+	<br />
+	<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">Our Premium Add-Ons!</h2>
+	<p style="text-align: center;">We have several new premium add-ons in our WordPress Store: MailChimp Integration (subscribes users to your Mailchimp list), Export Results (exports your quiz results), Extra Shortcodes (gives you extra shortcodes to use), User Dashboard (allow users to see the results from all the quizzes they have taken), and Advertisement Be Gone (gets rid of blue-border ads). Visit our <a href="http://mylocalwebstop.com/shop/">WordPress Store</a> for details! </p>
+	</div>
+	<div id="mlw_quiz_changelog" style="display: none;">
+	<h3><?php echo $mlw_quiz_version; ?> (August 11, 2014)</h3>
+	<ul>
+		<li>* Added Facebook Sharing</li>
+		<li>* Added Ability To Edit Email Subject</li>
+		<li>* Added Large Open Answer Question Type</li>
+		<li>* Added Ability For Points To Have Decimal Values</li>
+		<li>* Fixed Not All Questions Showing In Results Bug</li>
+		<li>* Fixed Not Valid Email On Non-Required Email Bug</li>
+		<li>* Fixed Special Character Not Grading Properly In Open Answer Bug</li>
+	</ul>
+	</div>
+	<div id="mlw_quiz_requested" style="display: none;">
+	<h3>Requested Features For Future Updates By Premium Support Users</h3>
+	<ul>
+		<li>None</li>
+	</ul>
+	<h3>Requested Features For Future Updates By Non-Premium Support Users</h3>
+	<ul>
+		<li>Importing Questions</li>
+		<li>Stats For Each Quiz</li>
+		<li>Force Login/Register Option</li>
+		<li>Categories</li>
+		<li>More Social Media Integration</li>
+		<li>Show Question Amount On Pagination</li>
+		<li>Required Questions</li>
+		<li>Duplicate Question</li>
+		<li>Allow Quiz To Not Show Start Page</li>
+		<li>Progress Bar For Timer</li>
+		<li>Ability To Redirect User Instead Of Showing Results Page</li>
+		<li>Multi-Delete Option For Quiz Results</li>
+		<li>Spam Prevention</li>
+	</ul>
+	</div>
+	</div>
+<?php
+}
+?>
